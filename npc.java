7c7520515f5a2a75150536c9334b83deda51db6e
LightVader23 <99289862+LightVader23@users.noreply.github.com>
2022-04-06 20:02:07 -0400

    Update npc.java

--- npc.java
+++ npc.java
@@ -1,1 +1,66 @@
 
+import java.util.Random;
+class NPC {
+     int healthpoints;
+     public int attack;
+     public int exp;
+     public int defense;
+
+
+     public int newHealth(int attack) {
+         healthpoints = healthpoints - attack;
+         return healthpoints;
+     }
+     public int defend(int attack){
+         attack=(defense- attack);
+         if(attack<0){
+             attack=0;
+         }
+         healthpoints=healthpoints-attack;
+         return healthpoints;
+     }
+public String run(){
+     String chance="";
+Random rand=new Random();
+int number=rand.nextInt(10)+1;
+if (number <5){
+    chance="fail";
+}
+if (number <5){
+    chance= "success";
+}
+         return chance;
+}
+     public int attack (){
+         return attack;
+     }
+     public int healthpoints (){
+         return healthpoints;
+     }
+     public int exp (){
+         return exp;
+     }
+     public int defense (){
+         return defense;
+     }
+
+ }
+
+
+
+ class Player extends NPC {
+     public Player(int attack, int healthpoints, int defense){
+       this.attack=attack;
+           this.healthpoints=healthpoints;
+             this.defense=defense;
+     }
+
+ }
+     class Enemies extends NPC {
+         public Enemies(int attack, int healthpoints, int defense){
+             this.attack=attack;
+           this.healthpoints=healthpoints;
+             this.defense=defense;
+         }
+        
+     }
