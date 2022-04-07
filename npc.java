4a2c4562ae600fd3ec0a8fb9afc7415a70a2bfde
LightVader23 <99289862+LightVader23@users.noreply.github.com>
2022-04-06 22:10:08 -0400

    Update npc.java

--- npc.java
+++ npc.java
@@ -1,70 +1,72 @@
+
 import java.util.Random;
 class NPC {
-    public int healthpoints;
-    public int attack;
-    public int exp;
-    public int defense;
+    private int healthpoints;
+    private int attack;
+    private int exp;
+    private int defense;
+
+    public NPC(int attack, int healthpoints, int defense) {
+        this.attack = attack;
+        this.healthpoints = healthpoints;
+        this.defense = defense;
+        exp = 1000;
+    }
 
 
     public int newHealth(int attack) {
         healthpoints = healthpoints - attack;
         return healthpoints;
     }
-    public int defend(int attack){
-        attack=(defense- attack);
-        if(attack<0){
-            attack=0;
+
+    public int defend(int attack) {
+        attack = (defense - attack);
+        if (attack < 0) {
+            attack = 0;
         }
-        healthpoints=healthpoints-attack;
+        healthpoints = healthpoints - attack;
         return healthpoints;
     }
-    public String run(){
-        String chance="";
-        Random rand=new Random();
-        int number=rand.nextInt(10)+1;
-        if (number <5){
-            chance="fail";
+
+    public String run() {
+        String chance = "";
+        Random rand = new Random();
+        int number = rand.nextInt(10) + 1;
+        if (number < 5) {
+            chance = "fail";
         }
-        if (number >=5){
-            chance= "success";
+        if (number >= 5) {
+            chance = "success";
         }
         return chance;
     }
-    public int attack (){
+
+    public int attack() {
         return attack;
     }
-    public int healthpoints (){
+
+    public int healthpoints() {
         return healthpoints;
     }
-    public int exp (){
+
+    public int exp() {
         return exp;
     }
-    public int defense (){
+
+    public int defense() {
         return defense;
     }
-    public int newexp (int exp){
-        this.exp=exp;
+
+    public int newexp(int exp) {
+        this.exp = exp;
         return exp;
-    }
-
-}
-
-
-
-class Player extends NPC {
-
-    public int expcost = 100;
-
-    public Player(int attack, int healthpoints, int defense) {
-        this.attack = attack;
-        this.healthpoints = healthpoints;
-        this.defense = defense;
     }
 
     public int newattack(int attack) {
         this.attack = attack + this.attack;
         return this.attack;
     }
+
 
     public int newhealth(int health) {
         healthpoints = health + healthpoints;
@@ -75,67 +77,76 @@ class Player extends NPC {
         this.defense = defense + this.defense;
         return this.defense;
     }
+}
 
-    public int newExpAmout() {
-        expcost = (int) (expcost * 1.2);
-        return expcost;
-    }
+    class Player extends NPC {
 
-    public void upgrade() {
-        System.out.print("You have " + exp);
-    }
+        public int expcost = 100;
 
-    public int upgrade(int choice) {
-        int exp = this.exp;
-        switch (choice) {
-            case 1:
-                if (exp < expcost) {
+        public Player(int attack, int healthpoints, int defense) {
+            super(attack, healthpoints, defense);
+
+        }
+
+
+        public int newExpAmout() {
+            expcost = (int) (expcost * 1.2);
+            return expcost;
+        }
+
+        public void upgrade() {
+            int exp= super.exp();
+            System.out.print("You have " + exp);
+        }
+
+        public int upgrade(int choice) {
+            int exp = super.exp();
+            switch (choice) {
+                case 1:
+                    if (exp < expcost) {
+                        break;
+                    }
+                    exp = exp - expcost;
+                    int attack = 1;
+
+                    newExpAmout();
+                    newexp(exp);
+                    super.newattack(attack);
                     break;
-                }
-                exp = exp - expcost;
-                int attack = 1;
+                case 2:
 
-                newExpAmout();
-                newexp(exp);
-                newattack(attack);
-                break;
-            case 2:
+                    if (exp < expcost) {
 
-                if (exp < expcost) {
+                        break;
+                    }
+                    exp = exp - expcost;
                     int defense = 1;
                     newExpAmout();
                     newexp(exp);
-                    newdefense(defense);
+                    super.newdefense(defense);
                     break;
-                }
-                exp = exp - expcost;
-                int defense = 1;
-                newExpAmout();
-                newexp(exp);
-                newdefense(defense);
-                break;
 
-            case 3:
-                if (exp < expcost) {
+                case 3:
+                    if (exp < expcost) {
+                        break;
+                    }
+                    exp = exp - expcost;
+                    int healthpoints = 1;
+                    newExpAmout();
+                    newexp(exp);
+                    super.newhealth(healthpoints);
                     break;
-                }
-                exp = exp - expcost;
-                int healthpoints = 1;
-                newExpAmout();
-                newexp(exp);
-                newhealth(healthpoints);
-                break;
-            default:
-                break;
-        }
-        return choice;
-    }
-}
-        class Enemies extends NPC {
-            public Enemies(int attack, int healthpoints, int defense){
-                this.attack=attack;
-                this.healthpoints=healthpoints;
-                this.defense=defense;
+                default:
+                    break;
             }
-
+            return choice;
         }
+    }
+
+    class Enemies extends NPC {
+        public Enemies(int attack, int healthpoints, int defense) {
+            super(attack, healthpoints, defense);
+        }
+
+
+    }
