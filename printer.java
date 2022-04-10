003a204a925a11eceedce27ebe01b13f86618699
a-nurmi <99289728+a-nurmi@users.noreply.github.com>
2022-04-10 19:55:08 -0400

    Update printer.java
    Final version of Printer class

--- printer.java
+++ printer.java
@@ -9,12 +9,11 @@ public class Printer {
 
     public static void Welcome(){ //prints welcome screen
         System.out.println("Welcome to Relinquished");
-        System.out.println("\nIn this game, you are an adventurer. You are lost in the woods and must find your way out." +
+        System.out.println("\nIn this game, you are an adventurer. You are lost in the woods and must find your way out.\n" +
                 "EXP is the currency in this game. You can choose to spend EXP or save up. " +
-                "\n However, be careful how you spend your EXP. You might earn more by saving as prices rise the farther you go."+
-                "\nYou must choose a path to follow...." +
-                "\n[Left]       [Center]       [Right]\n"  +
-                "\nType h to see the controls...");
+                "\nHowever, be careful how you spend your EXP. You will earn more EXP by saving. Prices in the upgrade shop will rise the farther you go." +
+                "\n\nNow lets begin the journey!"  +
+                "\nType h to see the controls:");
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
             if (input.equals("h")){
@@ -35,9 +34,8 @@ public class Printer {
         System.out.println("For Combat type: ");
         System.out.println("attack - to attack");
         System.out.println("defend - to defend");
-        System.out.println("run - to run\n");
-
-
+        System.out.println("run - to run");
     }
 
     }
+
