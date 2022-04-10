d8416eafcbfbf428f12d74d82b5e349d9d00a1e3
a-nurmi <99289728+a-nurmi@users.noreply.github.com>
2022-04-10 19:04:28 -0400

    Update printer.java

--- printer.java
+++ printer.java
@@ -9,27 +9,23 @@ public class Printer {
 
     public static void Welcome(){ //prints welcome screen
         System.out.println("Welcome to Relinquished");
-        System.out.println("\n[------------[R]------------]");
-        System.out.println("\nIn this game, you are an adventurer. " +
-                "\nYou are travelling to a far away kingdom that is said to be made" +
-                "\nof the purest gems of starlight. However, on the way, you entered " +
-                "\na deep, dark forest. At first, the path you followed was clear, but " +
-                "\nnow you've come to a fork in the road with three different paths:" +
-                "\n[Left]       [Center]       [Right]" +
+        System.out.println("\nIn this game, you are an adventurer. You are lost in the woods and must find your way out." +
+                "EXP is the currency in this game. You can choose to spend EXP or save up. " +
+                "\n However, be careful how you spend your EXP. You might earn more by saving as prices rise the farther you go."+
                 "\nYou must choose a path to follow...." +
-                "\n" +
+                "\n[Left]       [Center]       [Right]\n"  +
                 "\nType h to see the controls...");
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
             if (input.equals("h")){
-                Printer.Controls();}
+                Printer.help();}
         else{System.out.println("That's not a valid input");}
 
     }
 
-    public static void Controls(){ //prints controls
+    public static void help(){
 
-        System.out.println("[Controls]");
+        System.out.println("Press: ");
         System.out.println("f - forward");
         System.out.println("r - right");
         System.out.println("l - left");
@@ -39,7 +35,9 @@ public class Printer {
         System.out.println("For Combat type: ");
         System.out.println("attack - to attack");
         System.out.println("defend - to defend");
-        System.out.println("run - to run");
+        System.out.println("run - to run\n");
+
+
     }
 
     }
