6cc3a58616e072a3fc8c9c217d6543ccfaecc469
a-nurmi <99289728+a-nurmi@users.noreply.github.com>
2022-04-10 20:10:28 -0400

    Update controls.java
    Got rid of the help = new help

--- controls.java
+++ controls.java
@@ -28,58 +28,58 @@ public class controls{
     public int currentTurn(turncount t, Player player){
 
         Scanner sc = new Scanner(System.in);
-Enemies normal= new Enemies(4,8,5);
-Enemies boss=new Enemies(8,15,6);
-chest chest=new chest();
+        Enemies normal= new Enemies(4,8,5);
+        Enemies boss=new Enemies(8,15,6);
+        chest chest=new chest();
         String choice="";
 
-       int turn = 0;
-       Combat com=new Combat();
+        int turn = 0;
+        Combat com=new Combat();
         String cont="";
 
         while(turn < 10&& !cont.equals("lose")&&!cont.equals("win")) {
 
-do {
-    System.out.println("You have 3 paths in front of you. Where do you want to go?");
+            do {
+                System.out.println("You have 3 paths in front of you. Where do you want to go?");
 
-    choice = sc.nextLine();
+                choice = sc.nextLine();
 
-    if (choice.equals("f")) {
+                if (choice.equals("f")) {
 
-        System.out.println("You went forward.");
+                    System.out.println("You went forward.");
 
-    } else if (choice.equals("l")) {
-        System.out.println("You went left.");
+                } else if (choice.equals("l")) {
+                    System.out.println("You went left.");
 
-    } else if (choice.equals("r")) {
-        System.out.println("You went right.");
-
-
-    } else if (choice.equals("u")) {
+                } else if (choice.equals("r")) {
+                    System.out.println("You went right.");
 
 
-        player.upgrade();
-        int num = scanner();
-
-        player.upgrade(num);
-
-    } else if (choice.equals("s")) {
-
-        player.Store();
+                } else if (choice.equals("u")) {
 
 
-    }
-    else if(choice.equals("h")){
-        Printer.help();
-    }
-    if (!choice.equals("l")&&!choice.equals("u")&&!choice.equals("f")&&!choice.equals("r")&&!choice.equals("h")&&!choice.equals("s")){
-System.out.println("That is not a valid input");
-    }
-}
+                    player.upgrade();
+                    int num = scanner();
 
-    while (!choice.equals("l")&&!choice.equals("u")&&!choice.equals("f")&&!choice.equals("r")&&!choice.equals("h")&&!choice.equals("s"));
+                    player.upgrade(num);
 
-             if (choice.equals("u")){
+                } else if (choice.equals("s")) {
+
+                    player.Store();
+
+
+                }
+                else if(choice.equals("h")){
+                    Printer.help();
+                }
+                if (!choice.equals("l")&&!choice.equals("u")&&!choice.equals("f")&&!choice.equals("r")&&!choice.equals("h")&&!choice.equals("s")){
+                    System.out.println("That is not a valid input");
+                }
+            }
+
+            while (!choice.equals("l")&&!choice.equals("u")&&!choice.equals("f")&&!choice.equals("r")&&!choice.equals("h")&&!choice.equals("s"));
+
+            if (choice.equals("u")){
 
 
 
@@ -91,30 +91,30 @@ System.out.println("That is not a valid input");
                 continue;
 
             }
-             else if(choice.equals("h")){
-             continue;
-             }
+            else if(choice.equals("h")){
+                continue;
+            }
             turn++;
-             chest.Chest(player);
-player.multiply();
+            chest.Chest(player);
+            player.multiply();
 
-    if(!cont.equals("lose")){
-    if(turn==2){
-    cont=com.battle(player,normal,0);
-}
+            if(!cont.equals("lose")){
+                if(turn==2){
+                    cont=com.battle(player,normal,0);
+                }
 
-    else if(turn==4){
-        cont=com.battle(player,normal,0);
-    }
-   else if(turn==6){
-        cont=com.battle(player,normal,0);
-    }
-    else if(turn==8){
-        cont=com.battle(player,normal,0);
-    }
-      else if(turn==10){
-       cont=com.battle(player,boss,1);
-    }
+                else if(turn==4){
+                    cont=com.battle(player,normal,0);
+                }
+                else if(turn==6){
+                    cont=com.battle(player,normal,0);
+                }
+                else if(turn==8){
+                    cont=com.battle(player,normal,0);
+                }
+                else if(turn==10){
+                    cont=com.battle(player,boss,1);
+                }
 
             }
 
@@ -180,8 +180,6 @@ player.multiply();
         t.setTurn(a);
 
         return t.getTurn();
-
-
-
+        
     }}
 
